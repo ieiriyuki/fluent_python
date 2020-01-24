d62800172003c577e5c3307e994e0f11ecd11bef
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2020-01-25 08:13:16 +0900

    Add example 2-11 is added

---
+++ src/eg2_11.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+invoice = """
+0.....6.................................40..........52...55........
+1909  Prmoroni PiBrella                        $17.50    3    $52.50
+1489  6mm Tactile Switch x 20                   $4.05    2     $9.90
+1510  Panavise Jr. - PV-201                    $28.00    1    $28.00
+1601  PiTFT Mini Kit 320x240                   $34.95    1    $34.95
+"""
+
+
+def main():
+    SKU = slice(0, 6)
+    DESCRIPTION = slice(6, 40)
+    UNIT_PRICE = slice(40, 52)
+    QUANTITY = slice(52, 55)
+    ITEM_TOTAL = slice(55, None)
+    line_items = invoice.split('\n')[2:]
+    for item in line_items:
+        print(item[UNIT_PRICE], item[DESCRIPTION])
+
+    return 1
+
+
+if __name__ == '__main__':
+    main()
