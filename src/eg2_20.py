f6c40ea752f72e3d53cf606080fd3201831e5687
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2020-01-27 20:42:47 +0900

    Add example 2-20 is added

---
+++ src/eg2_20.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from array import array
+from random import random
+
+
+floats = array('d', (random() for i in range(10 ** 7)))
+print(floats[-1])
+
+with open("floats.bin", "wb") as fp:
+    floats.tofile(fp)
+
+floats2 = array("d")
+with open("floats.bin", "rb") as fp:
+    floats2.fromfile(fp, 10**7)
+
+print(floats2[-1])
+
+print(floats2 == floats)
